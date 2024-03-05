282d312c1f91844524ff55c2abcf1116df300c98
William Santana <euwilliamsantana@gmail.com>
2024-03-05 09:57:52 -0300

    feat: Layout SignIn + Form + validation

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,12 +1,21 @@
+import { Pizza } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
   return (
-    <>
-      <h1>Auth Layout</h1>
-      <div>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+        <div className="flex items-center gap-3 text-lg text-foreground">
+          <Pizza size={20} />
+          <span className="font-semibold">pizza.shop</span>
+        </div>
+        <footer className="text-sm">
+          Painel do parceiro &copy; pizza.shop
+        </footer>
+      </div>
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
-    </>
+    </div>
   )
 }
--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,9 +1,55 @@
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+const signInSchema = z.object({
+  email: z.string().email(),
+})
+
+type SignInProps = z.infer<typeof signInSchema>
 
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInProps>()
+
+  async function handleSignIn(data: SignInProps) {
+    console.log(data)
+
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+  }
+
   return (
     <>
-      <h1>SignIn</h1>
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar painel
+            </h1>
+            <p className="text-sm text-muted-foreground ">
+              Acompanhe suas vendas pelo painel do parceiro!
+            </p>
+          </div>
+
+          <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input type="email" id="email" {...register('email')} />
+            </div>
+
+            <Button className="w-full" type="submit" disabled={isSubmitting}>
+              Acessar painel
+            </Button>
+          </form>
+        </div>
+      </div>
       <Helmet title="SignIn" />
     </>
   )
