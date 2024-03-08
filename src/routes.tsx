375920c5136150150c96e1c3bd7e544594b1b798
William Santana <euwilliamsantana@gmail.com>
2024-03-08 08:12:03 -0300

    feat: Page Dash Finalized + Page NotFound Error

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,11 +6,13 @@ import { Dashboard } from './pages/app/dashboard/dashboard'
 import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
+import { NotFound } from './pages/error'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       { path: '/', element: <Dashboard /> },
       { path: '/order', element: <Orders /> },
