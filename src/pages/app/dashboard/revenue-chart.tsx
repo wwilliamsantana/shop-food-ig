0bcc444b15c381253529c196a8b4242a67f904a6
William Santana <euwilliamsantana@gmail.com>
2024-03-08 07:42:13 -0300

    feat: Charts using recharts

---
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -0,0 +1,62 @@
+import { Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts'
+import colors, { violet } from 'tailwindcss/colors'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+const data = [
+  { date: '01/03', revenue: 1200 },
+  { date: '02/03', revenue: 200 },
+  { date: '03/03', revenue: 800 },
+  { date: '04/03', revenue: 50 },
+  { date: '05/03', revenue: 3000 },
+  { date: '06/03', revenue: 400 },
+  { date: '07/03', revenue: 1500 },
+  { date: '08/03', revenue: 1000 },
+]
+
+export function RevenueChart() {
+  return (
+    <Card className="col-span-6">
+      <CardHeader className="j flex-row items-center pb-8">
+        <div className="space-y-1">
+          <CardTitle className="text-base font-medium">
+            Receita no período
+          </CardTitle>
+          <CardDescription>Receieta diária no período</CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <LineChart style={{ fontSize: 12 }} data={data}>
+            <XAxis dataKey="date" axisLine={false} tickLine={false} dy={16} />
+            <YAxis
+              stroke="#888"
+              axisLine={false}
+              tickLine={false}
+              width={80}
+              tickFormatter={(value: number) =>
+                value.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                })
+              }
+            />
+
+            <Line
+              type="linear"
+              strokeWidth={2}
+              dataKey="revenue"
+              stroke={colors.violet[500]}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  )
+}
