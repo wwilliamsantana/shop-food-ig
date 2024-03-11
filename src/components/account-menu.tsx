3401a407edcca94c883d2ac4a774fa040b199f45
William Santana <euwilliamsantana@gmail.com>
2024-03-11 10:37:28 -0300

    feat: Skeleton - AccountMenu Component

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -13,17 +13,19 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from './ui/dropdown-menu'
+import { Skeleton } from './ui/skeleton'
 
 export function AccountMenu() {
-  const { data: profile } = useQuery({
+  const { data: profile, isLoading: isLoadingProfile } = useQuery({
     queryKey: ['profile'],
     queryFn: getProfile,
   })
 
-  const { data: managerRestaurant } = useQuery({
-    queryKey: ['manager-restaurant'],
-    queryFn: getManagerRestaurant,
-  })
+  const { data: managerRestaurant, isLoading: isLoadingManagerRestaurant } =
+    useQuery({
+      queryKey: ['manager-restaurant'],
+      queryFn: getManagerRestaurant,
+    })
 
   return (
     <DropdownMenu>
@@ -32,16 +34,29 @@ export function AccountMenu() {
           variant={'outline'}
           className="flex select-none items-center gap-2"
         >
-          {managerRestaurant?.name}
+          {isLoadingManagerRestaurant ? (
+            <Skeleton className="h-4 w-40" />
+          ) : (
+            managerRestaurant?.name
+          )}
           <ChevronDown size={16} />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col">
-          <span>{profile?.name}</span>
-          <span className="text-xs font-normal text-muted-foreground">
-            {profile?.email}
-          </span>
+          {isLoadingProfile ? (
+            <div className="space-y-1.5">
+              <Skeleton className="h-4 w-32" />
+              <Skeleton className="h-3 w-24" />
+            </div>
+          ) : (
+            <>
+              <span>{profile?.name}</span>
+              <span className="text-xs font-normal text-muted-foreground">
+                {profile?.email}
+              </span>
+            </>
+          )}
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
 
