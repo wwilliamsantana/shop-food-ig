8a935606796bbf1b0d1811d6fad5260423f13454
William Santana <euwilliamsantana@gmail.com>
2024-04-03 20:47:00 -0300

    config mocks

---
+++ src/api/mocks/sing-in-mock.ts
@@ -0,0 +1,21 @@
+import { http, HttpResponse } from 'msw'
+
+import { SignInProps } from '../signIn'
+
+export const signInMock = http.post<never, SignInProps>(
+  '/authenticate',
+  async ({ request }) => {
+    const { email } = await request.json()
+
+    if (email === 'john@example.com') {
+      return new HttpResponse(null, {
+        status: 200,
+        headers: {
+          'Set-Cookie': 'auth=sample-jwt',
+        },
+      })
+    }
+
+    return new HttpResponse(null, { status: 401 })
+  },
+)
--- src/api/signIn.ts
+++ src/api/signIn.ts
@@ -1,6 +1,6 @@
 import { api } from '@/lib/axios'
 
-interface SignInProps {
+export interface SignInProps {
   email: string
 }
 
